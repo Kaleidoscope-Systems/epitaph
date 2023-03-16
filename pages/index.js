8caf2722ab6415035146b8c1091563fd6ebc9057
Steven Carlson <mail@stevencarlson.me>
2023-03-15 22:43:53 -0600

    Use variable for page title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Ss. Nicodemus & Joseph Burial Society</title>
+        <title>{process.env.NEXT_PUBLIC_SOCIETY_SHORT_NAME}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Ss. Nicodemus and Joseph Burial Society of Northern Colorado" />
         <link rel="icon" href="/favicon.ico" />
