7620a2e71b33c23a1a55eda3ef5175746b3d0d03
Steven Carlson <mail@stevencarlson.me>
2023-03-18 21:14:15 -0600

    Put in a container

--- pages/about-us.js
+++ pages/about-us.js
@@ -13,12 +13,14 @@ export default function About() {
       </Head>
 
       <main className="text-bg-light p-4 rounded-5">
+        <div className='container-lg'>
         <h1>
             About Us
         </h1>
         <p>
             <strong>The Ss. Nicodemus & Joseph Orthodox Christian Burial Society of Northern Colorado</strong> was established in 2013 to assist those desiring to follow Ancient Orthodox Christian Practices in pre-planning their burial or that of a loved one. It is a Pan-Orthodox society made up of Orthodox volunteers from Northern Colorado who view the society’s work as a ministry to the grieving and a service to departed Christian brothers and sisters in preparing them to attend the heavenly banquet with Christ their King and Savior.
         </p>
+        </div>
       </main>
     </div>
     )
