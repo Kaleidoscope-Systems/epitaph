aa519046926139e057da9a0c1832a63adf022a19
Steven Carlson <mail@stevencarlson.me>
2023-06-07 19:24:57 -0600

    Remove whitespace

--- components/Nav.js
+++ components/Nav.js
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import LoginBtn from './login-btn';
 
-function GlobalNav() { 
+function GlobalNav() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
