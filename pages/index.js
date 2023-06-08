a6ba3660bbfa4dfebc4b70a00239ba1987547463
Steven Carlson <mail@stevencarlson.me>
2023-06-07 19:23:54 -0600

    Remove whitespace

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 //import Image from 'next/image'
 
-export default function Home() { 
+export default function Home() {
   return (
     <div className="container">
       <Head>
