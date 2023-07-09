0256f4dd1961a8a986cc1661bad97193fef131ef
Steven Carlson <mail@stevencarlson.me>
2023-07-08 20:31:37 -0600

    Add Font Awesome

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,9 @@ import { SSRProvider } from 'react-bootstrap';
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import { SessionProvider } from "next-auth/react"
 import CookieConsent from "react-cookie-consent";
+import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 const App = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
