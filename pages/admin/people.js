02993db910e4da79ba8307a78b1d814a0c2d8e02
Steven Carlson <mail@stevencarlson.me>
2023-07-08 22:05:43 -0600

    Add initial admin pages

--- pages/admin/people.js
+++ pages/admin/people.js
@@ -2,14 +2,14 @@ import Head from 'next/head'
 import {useSession} from "next-auth/react"
 import AdminSidebar from '@/components/admin-sidebar'
 
-export default function Dashboard() {
+export default function Records() {
 	const { data: session } = useSession()
 	if (!session) return
 	if (session) {
 		return (
 			<>
 				<Head>  
-				<title>Dashboard - Ss. Nicodemus & Joseph Burial Society</title>
+				<title>People - Ss. Nicodemus & Joseph Burial Society</title>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta name="description" content="Ss. Nicodemus and Joseph Burial Society of Northern Colorado" />
 				<link rel="icon" href="/favicon.ico" />
