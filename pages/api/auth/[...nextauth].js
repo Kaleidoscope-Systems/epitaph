be2d65e68cd89249ad1a71781e80fba30d9c4bbd
Steven Carlson <mail@stevencarlson.me>
2023-07-14 21:19:50 -0600

    Inital test of account creation

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
