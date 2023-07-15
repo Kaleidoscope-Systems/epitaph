95de80292f5ca7d904bf66e695f83edea3ecca59
Steven Carlson <mail@stevencarlson.me>
2023-07-14 20:41:45 -0600

    Simplify code

--- components/access-denied.jsx
+++ components/access-denied.jsx
@@ -10,7 +10,7 @@ export default function AccessDenied() {
 				<div className="row position-absolute top-50 start-50 translate-middle">
 					<h1>Access Denied</h1>
 					<div className="col col-12">
-						<div className="pb-1">You must sign in to access this page.</div><br/>
+						<div className="pb-1">{!session ? "You must sign in to access this page." : "You do not have access to this page. Please contact the administrator if this doesn't sound right."}</div><br/>
 						<LoginBtn />
 					</div>
 				</div>
@@ -18,20 +18,4 @@ export default function AccessDenied() {
 		</main>
 		</>
 	)
-	if (session) return(
-		<>
-		<main className="d-flex flex-nowrap" style={{height: '100vh'}}>
-			<div className="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style={{width: '100%'}}>
-			<div className="row position-absolute top-50 start-50 translate-middle">
-					<h1>Access Denied</h1>
-					<div className="col col-12">
-					<div className="pb-1">You do not have access to this page. Please contact the administrator if this doesn&apos;t sound right.</div><br/>
-						<LoginBtn />
-					</div>
-				</div>
-			</div>
-		</main>
-		</>
-	)
-	
 }
