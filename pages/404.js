24918cee511bb5745b0c9d5b9a6690e8e221b7d9
Steven Carlson <mail@stevencarlson.me>
2023-07-15 21:44:53 -0600

    Nevermind, this H1 actually matches the homepage

--- pages/404.js
+++ pages/404.js
@@ -9,7 +9,7 @@ export default function Custom404() {
 			<meta name="description" content="Ss. Nicodemus and Joseph Burial Society of Northern Colorado" />
 			<link rel="icon" href="/favicon.ico" />
 		</Head>
-        <h1>404 - Page Not Found</h1>
+        <h1 className="display-5 fw-bold">404 - Page Not Found</h1>
         <p className="col-md-8 fs-4">The page you are looking for does not exist.</p>
         <Link className="btn btn-primary btn-lg" role="button" href="/">Return to home page</Link>        
     </div>     
