9bc3b44ba21a5b18e3269dfc4d51f7e5c9f73f52
Steven Carlson <mail@stevencarlson.me>
2023-07-31 19:08:37 -0600

    Add Bootstrap JS

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,13 @@ import { SessionProvider } from "next-auth/react"
 import CookieConsent from "react-cookie-consent";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { useEffect } from "react";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 const App = ({ Component, pageProps: { session, ...pageProps } }) => {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
   return (
     <>
       <GoogleAnalytics trackPageViews />
