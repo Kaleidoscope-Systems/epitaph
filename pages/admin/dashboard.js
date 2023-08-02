f32c658b48c51a80000b9d4b69482a54bcbd9e55
Steven Carlson <mail@stevencarlson.me>
2023-08-01 22:20:21 -0600

    Use Loading component

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import {useSession} from "next-auth/react"
 import AdminSidebar from '@/components/admin-sidebar'
 import AccessDenied from '@/components/access-denied'
+import Loading from '@/components/loading'
 
 export default function Dashboard() {
 	const { data: session, status } = useSession()
@@ -22,16 +23,7 @@ export default function Dashboard() {
 			<meta name="description" content="Ss. Nicodemus and Joseph Burial Society of Northern Colorado" />
 			<link rel="icon" href="/favicon.ico" />
 		</Head><AccessDenied /></>)
-	if ('loading' === status) return (<>
-		<div className="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style={{width: '100%'}}>
-			<h2>Loading...</h2>
-			<div className="row">
-				<div className="col col-12">
-					
-				</div>
-			</div>
-		</div></>
-	)
+	if ('loading' === status) return (<Loading />)
 	if (session && 'authenticated' === status) return (
 		<><Head>
 			<title>Dashboard - Ss. Nicodemus & Joseph Burial Society</title>
