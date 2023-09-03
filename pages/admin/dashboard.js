9cc9f8aafab17732a2f7e9e459b1c3cc6ed0d071
Steven Carlson <mail@stevencarlson.me>
2023-09-02 20:09:13 -0600

    Fix permissions for the dashboard

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -6,19 +6,19 @@ import Loading from '@/components/loading'
 
 export default function Dashboard() {
 	const { data: session, status } = useSession()
-	const caps =
+	const caps = 
 		session && session.user.caps
 			? (() => {
 					try {
-						return JSON.parse(session.user.caps);
+						return session.user.caps;
 					} catch (e) {
-						console.error('Parse error', session.user.caps);
+						console.error('Error getting user capabilities:', e);
 						return {};
 					}
 				})()
 			: {};
 	if ('loading' === status) return (<Loading />)
-	if ('unauthenticated' === status) return (
+	if ('unauthenticated' === status && caps.viewDashboard) return (
 		<><Head>
 			<title>Access Denied - Ss. Nicodemus & Joseph Burial Society</title>
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -44,4 +44,27 @@ export default function Dashboard() {
 			</div>
 		</main></>
 	)
+	if ('authenticated' === status && !caps.viewDashboard) {
+		return (
+			<>
+				<Head>
+				<title>Dashboard - Ss. Nicodemus & Joseph Burial Society</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="description" content="Ss. Nicodemus and Joseph Burial Society of Northern Colorado" />
+				<link rel="icon" href="/favicon.ico" />
+				</Head>
+				<main className="d-flex flex-nowrap" style={{height: '100vh'}}>
+					<AdminSidebar />
+					<div className="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style={{width: '100%'}}>
+						<div className="row position-absolute top-50 start-50 translate-middle">
+							<h1>Access Denied</h1>
+							<div className="col col-12">
+								<div className="pb-1">You do not have permission to view the dashboard.</div><br/>
+							</div>
+						</div>
+					</div>
+				</main>
+			</>
+		)
+	}
 }
