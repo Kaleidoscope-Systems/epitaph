c5a656e7d746b4209b3a747b14c4cf7c323d3830
Steven Carlson <mail@stevencarlson.me>
2023-09-06 21:03:16 -0600

    Import Layout instead of Head

--- pages/planning-discussion-guide.js
+++ pages/planning-discussion-guide.js
@@ -1,66 +1,60 @@
-import Head from 'next/head'
 import Image from 'react-bootstrap/Image'
 import styles from '../styles/Layout.module.css'
+import Layout from '@/components/Layout'
 
 export default function About() {
+    const appModule = "home";
     return (
-        <div>
-      <Head>
-        <title>Planning & Discussion Guide - Ss. Nicodemus & Joseph Burial Society</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Ss. Nicodemus and Joseph Burial Society of Northern Colorado" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="text-bg-light p-4 rounded-5">
-        <h1>
-            Planning & Discussion Guide
-        </h1>
-        <h2>
-            Our Primary Support
-        </h2>
-        <p>
-        The Ss. Nicodemus & Joseph Orthodox Christian Burial Society was formed to offer ancient Christian burial practices to those who desire that in Northern Colorado.
-        </p>
-        <ul>
-            <li><strong>Practical Support</strong> — help offered for many decisions: casket, funeral home (or not), cemetery, clothing, transport, caterer, communication, obituary, some basic legalities, etc.</li>
-            <li><strong>Psalms</strong> — Orthodox tradition calls for Psalms to be read as the soul separates from the body. God never intended for the soul and the body to be separated, so the Psalms provide aid to this difficult process. A Psalm Reading email goes out to the parish and to any email addresses provided by the family in advance.</li>
-            <li><strong>Prayer</strong> — The team usually accompanies the priest on the initial call to say the final prayers and begin the Psalms. The team will also immediately notify the Myrrhbearers Ministry for their prayers. Incense is traditional with Orthodox prayer but can be omitted with request.</li>
-            <li><strong>Meals/Reception</strong> by Philoptochos or other ministries
-                <ul>
-                <li>Family Meals—Support Angels can supply upon request</li>
-                <li>Light Reception — after Trisagion Prayers (night before funeral)</li>
-                <li>Makaria/Mercy Meal — Advice or assistance in planning or helping serve the traditional Fish Meal of Blessing or Makaria, offered by the family after the funeral/burial</li>
-                <li><em>Family may accept or decline any of these in advance or will be contacted at the time.</em></li>
-                <li>Some Financial Aid may available as well.</li>
-                </ul>
-            </li>
-        </ul>
-        <h2>Other Support & Plans</h2>
-        <p>An Orthodox Funeral & Burial are viewed as a holy sending up of the soul as soon after passing as possible. Our goal is to prepare the beloved for their heavenly banquet with the King of Kings in whatever ways we can.</p>
-        <p><strong>Preparation of the Body</strong>, is done in a prayerful, private, &amp; dignified manner at home, care facility, funeral home, etc. The covered body is cleansed, anointed, and prepared for transport. If using a Funeral Home, they will transport and use a cooler until the funeral. If the family wishes to keep the body at home until the funeral, other methods for cooling the body can be employed by the family, with guidance by the society. Dressing can be done by the team or a funeral home. If under refrigeration, final dressing will occur just prior to the funeral. If at home, under simple cooling, dressing can happen at preparation.</p>
-        <h2>Suggestions for Planning Ahead</h2>
-        <ul>
-            <li>Create a Family Notification Plan in advance</li>
-            <li>Write an Obituary in advance</li>
-            <li>Choose clothing for burial (and your own funeral attire)</li>
-            <li>Decisions for Living Will, Organ Donation etc.</li>
-            <li>Decide on the use of a funeral home. If not, then plan for transport &amp; placement of body prior to Trisagion. Society may be able to help arrange or advise on transport and cooling.</li>
-            <li>Traditionally, the Orthodox prefer a casket that will allow the body to follow its natural return to the earth from where it was taken (Gen. 2:7). If using a Funeral Home, an unfinished Jewish Casket can provide such an environment. Caskets are available online, but avoid any described as ‘sealed’—since this does not allow natural burial. Some church craftsmen also make them. Caskets are open at Orthodox funerals, with some exceptions.</li>
-            <li>Grave Liners are open to the soil and are much less expensive than vaults. Polymer ones are the least expensive and supply all needed support. But permission often required at some cemeteries.</li>
-            <li>Transport: permit needed to transport out of state, (via airline, car, etc.). Some states require embalming or other details.</li>
-        </ul>
-        <h2>Steps at Repose</h2>
-        <ul>
-            <li>Contact the Priest first</li>
-            <li>Begin your Family/Friends notification system</li>
-            <li>Remind Hospice, if they are involved, that the Burial Team will be doing the preparation, cleansing &amp; anointing, so they should not begin.</li>
-            <li>Expect some members of the Burial Society team to arrive with Father. Others may arrive shortly thereafter. We will begin the Psalm reading, Prayers, (with the traditional incense unless otherwise directed) and the preparation of the body in a private, prayerful, and dignified manner. If the room of death is not private you may request a privacy screen, at initial call.</li>
-            <li>Contact the Funeral Home if they are to transport the body. Remind them that the team is preparing the body, and there will be no embalming, only cooling. They will be familiar with this part, as it is also done by other faiths, though a reminder is best.</li>
-            <li>The team will have already started the Psalm Reading, but we will need to know the funeral service details, as soon as possible, to generate the Psalm Reading link that is emailed to all the Church.</li>
-            <li>Contact any special entities: i.e. transport, etc.</li>
-        </ul>
-      </main>
-    </div>
+      <Layout title="Planning & Discussion Guide" metaDescriptionContent={process.env.NEXT_PUBLIC_SOCIETY_LONG_NAME} appModule={appModule}>
+        <main className="text-bg-light p-4 rounded-5">
+          <h1>
+              Planning & Discussion Guide
+          </h1>
+          <h2>
+              Our Primary Support
+          </h2>
+          <p>
+          The Ss. Nicodemus & Joseph Orthodox Christian Burial Society was formed to offer ancient Christian burial practices to those who desire that in Northern Colorado.
+          </p>
+          <ul>
+              <li><strong>Practical Support</strong> — help offered for many decisions: casket, funeral home (or not), cemetery, clothing, transport, caterer, communication, obituary, some basic legalities, etc.</li>
+              <li><strong>Psalms</strong> — Orthodox tradition calls for Psalms to be read as the soul separates from the body. God never intended for the soul and the body to be separated, so the Psalms provide aid to this difficult process. A Psalm Reading email goes out to the parish and to any email addresses provided by the family in advance.</li>
+              <li><strong>Prayer</strong> — The team usually accompanies the priest on the initial call to say the final prayers and begin the Psalms. The team will also immediately notify the Myrrhbearers Ministry for their prayers. Incense is traditional with Orthodox prayer but can be omitted with request.</li>
+              <li><strong>Meals/Reception</strong> by Philoptochos or other ministries
+                  <ul>
+                  <li>Family Meals—Support Angels can supply upon request</li>
+                  <li>Light Reception — after Trisagion Prayers (night before funeral)</li>
+                  <li>Makaria/Mercy Meal — Advice or assistance in planning or helping serve the traditional Fish Meal of Blessing or Makaria, offered by the family after the funeral/burial</li>
+                  <li><em>Family may accept or decline any of these in advance or will be contacted at the time.</em></li>
+                  <li>Some Financial Aid may available as well.</li>
+                  </ul>
+              </li>
+          </ul>
+          <h2>Other Support & Plans</h2>
+          <p>An Orthodox Funeral & Burial are viewed as a holy sending up of the soul as soon after passing as possible. Our goal is to prepare the beloved for their heavenly banquet with the King of Kings in whatever ways we can.</p>
+          <p><strong>Preparation of the Body</strong>, is done in a prayerful, private, &amp; dignified manner at home, care facility, funeral home, etc. The covered body is cleansed, anointed, and prepared for transport. If using a Funeral Home, they will transport and use a cooler until the funeral. If the family wishes to keep the body at home until the funeral, other methods for cooling the body can be employed by the family, with guidance by the society. Dressing can be done by the team or a funeral home. If under refrigeration, final dressing will occur just prior to the funeral. If at home, under simple cooling, dressing can happen at preparation.</p>
+          <h2>Suggestions for Planning Ahead</h2>
+          <ul>
+              <li>Create a Family Notification Plan in advance</li>
+              <li>Write an Obituary in advance</li>
+              <li>Choose clothing for burial (and your own funeral attire)</li>
+              <li>Decisions for Living Will, Organ Donation etc.</li>
+              <li>Decide on the use of a funeral home. If not, then plan for transport &amp; placement of body prior to Trisagion. Society may be able to help arrange or advise on transport and cooling.</li>
+              <li>Traditionally, the Orthodox prefer a casket that will allow the body to follow its natural return to the earth from where it was taken (Gen. 2:7). If using a Funeral Home, an unfinished Jewish Casket can provide such an environment. Caskets are available online, but avoid any described as ‘sealed’—since this does not allow natural burial. Some church craftsmen also make them. Caskets are open at Orthodox funerals, with some exceptions.</li>
+              <li>Grave Liners are open to the soil and are much less expensive than vaults. Polymer ones are the least expensive and supply all needed support. But permission often required at some cemeteries.</li>
+              <li>Transport: permit needed to transport out of state, (via airline, car, etc.). Some states require embalming or other details.</li>
+          </ul>
+          <h2>Steps at Repose</h2>
+          <ul>
+              <li>Contact the Priest first</li>
+              <li>Begin your Family/Friends notification system</li>
+              <li>Remind Hospice, if they are involved, that the Burial Team will be doing the preparation, cleansing &amp; anointing, so they should not begin.</li>
+              <li>Expect some members of the Burial Society team to arrive with Father. Others may arrive shortly thereafter. We will begin the Psalm reading, Prayers, (with the traditional incense unless otherwise directed) and the preparation of the body in a private, prayerful, and dignified manner. If the room of death is not private you may request a privacy screen, at initial call.</li>
+              <li>Contact the Funeral Home if they are to transport the body. Remind them that the team is preparing the body, and there will be no embalming, only cooling. They will be familiar with this part, as it is also done by other faiths, though a reminder is best.</li>
+              <li>The team will have already started the Psalm Reading, but we will need to know the funeral service details, as soon as possible, to generate the Psalm Reading link that is emailed to all the Church.</li>
+              <li>Contact any special entities: i.e. transport, etc.</li>
+          </ul>
+        </main>
+      </Layout>
     )
 }
