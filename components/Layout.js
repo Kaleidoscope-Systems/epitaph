6d9a30df9e693bd562ed8229e3a79dc2d5bfb5bb
Steven Carlson <mail@stevencarlson.me>
2023-09-06 20:21:27 -0600

    Add metaDescriptionContent

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,8 @@ import styles from '../styles/Layout.module.css'
 import adminStyles from '../styles/LayoutAdmin.module.css'
 import { useRouter } from 'next/router'
 
-const Layout = ({children, title = 'Ss. Nicodemus & Joseph Burial Society', appModule}) => {
+
+const Layout = ({children, title, metaDescriptionContent, appModule}) => {
     const router = useRouter()
     const slug = router.pathname;
     if (slug.includes("admin", 1)) {
@@ -14,7 +15,7 @@ const Layout = ({children, title = 'Ss. Nicodemus & Joseph Burial Society', appM
         <Head>
 			<title>{title}</title>
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
-			<meta name="description" content="Ss. Nicodemus and Joseph Burial Society of Northern Colorado" />
+			<meta name="description" content={metaDescriptionContent} />
 			<link rel="icon" href="/favicon.ico" />
 		</Head>
         <GlobalNav appModule={appModule} /><div className={adminStyles.container}>
