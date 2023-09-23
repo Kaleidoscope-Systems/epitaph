65bf6f70ce22e728c086561d5895577b29c408a9
Steven Carlson <mail@stevencarlson.me>
2023-09-22 22:15:30 -0600

    Protect API route

--- pages/api/v1/people/[id].js
+++ pages/api/v1/people/[id].js
@@ -1,8 +1,23 @@
 import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../../auth/[...nextauth]";
 
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
+  const session = await getServerSession(req, res, authOptions)
+  const caps = 
+		session && session.user.caps
+			? (() => {
+					try {
+						return session.user.caps;
+					} catch (e) {
+						console.error('Error getting user capabilities:', e);
+						return {};
+					}
+				})()
+			: {};
+  if (session && caps.viewPeople) {
     try {
       const { id } = req.query;
       const peopleResponse = await prisma.people.findUniqueOrThrow({
@@ -13,6 +28,13 @@ export default async function handler(req, res) {
       );
       res.status(200).json(peopleResponse);
     } catch (error) {
-        res.status(500).json(error)
+      res.status(500).json(error)
     }
+  }
+  if (session && !caps.viewPeople) {
+    res.status(403).json({error: 'User does not have permission to view people.'})
+  }
+  if (!session) {
+    res.status(401).json({error: 'You must first authenticate.'})
+  }  
 }
