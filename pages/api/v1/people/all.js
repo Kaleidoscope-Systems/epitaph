a0ff32f991b636b29b224e71798467cd3a7b06ff
Steven Carlson <mail@stevencarlson.me>
2024-09-08 20:53:02 -0600

    Migrate all endpoint to pg

--- pages/api/v1/people/all.js
+++ pages/api/v1/people/all.js
@@ -1,4 +1,4 @@
-import prisma from "@/lib/prisma";
+import pg from 'pg';
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../../auth/[...nextauth]";
 
@@ -16,7 +16,35 @@ export default async function handler(req, res) {
 				})()
 			: {};
   if (session && caps.viewPeople) {
+    const { Client } = pg
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL,
+    });
+    await client.connect()
+    
+    const sql = `
+      SELECT p.*
+      FROM "people" p
+    `;
+
     try {
+      const peopleResponse = await client.query(sql);
+      const people = peopleResponse.rows;
+      res.status(200).json(people);
+    } catch (error) {
+      // Log the error for internal debugging
+      console.error("Error fetching people:", error);
+
+      // Send a user-friendly error message
+      res.status(500).json({ message: 'Error fetching data from the database' });
+    } finally {
+      try {
+        await client.end(); // Ensure the connection is properly closed
+      } catch (disconnectionError) {
+        console.error("Error disconnecting from the database:", disconnectionError);
+      }
+    }
+    /* try {
       //TODO: paginate results
       const peopleResponse = await prisma.people.findMany({
         include: { executor: true },
@@ -24,7 +52,7 @@ export default async function handler(req, res) {
       res.status(200).json(peopleResponse);
     } catch (error) {
       res.status(500).json(error)
-    }
+    } */
   }
   if (session && !caps.viewPeople) {
     res.status(403).json({error: 'User does not have permission to view people.'})
