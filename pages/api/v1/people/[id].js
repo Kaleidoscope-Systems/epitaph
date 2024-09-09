b670b3e71af3c7a94fa5fc96479b4bc7b6508286
Steven Carlson <mail@stevencarlson.me>
2024-09-08 20:53:40 -0600

    Migrate GET request to pg

--- pages/api/v1/people/[id].js
+++ pages/api/v1/people/[id].js
@@ -1,6 +1,36 @@
 import prisma from "@/lib/prisma";
+import pg from 'pg';
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../../auth/[...nextauth]";
+
+async function findPersonById(id) {
+  const { Client } = pg
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  try {
+    await client.connect();
+
+    const sql = `
+      SELECT *
+      FROM people
+      WHERE id = $1
+    `;
+
+    const values = [id];
+
+    const result = await client.query(sql, values);
+
+    if (result.rows.length === 0) {
+      throw new Error('Person not found');
+    }
+
+    return result.rows[0];
+  } finally {
+    await client.end();
+  }
+}
 
 export default async function handler(req, res) {
   const session = await getServerSession(req, res, authOptions)
@@ -32,13 +62,8 @@ export default async function handler(req, res) {
   if (req.method === 'GET' && session && caps.viewPeople) {
     try {
       const { id } = req.query;
-      const peopleResponse = await prisma.people.findUniqueOrThrow({
-        where: {
-          id: id,
-        },
-      },
-      );
-      res.status(200).json(peopleResponse);
+      const person = await findPersonById(id);
+      res.status(200).json(person);
     } catch (error) {
       res.status(500).json(error)
     }
